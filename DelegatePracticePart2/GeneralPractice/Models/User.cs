a07e4ff01a53f38ce3a396149788bf495f412ca7
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-30 15:16:59 +0400

    first commit

---
+++ DelegatePracticePart2/GeneralPractice/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using GeneralPractice.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils.Enums;
+
+namespace GeneralPractice.Models
+{
+    internal class User : IEntity
+    {
+        private static int _id;
+        public int Id { get; }
+        public Role Role { get; set; }
+
+
+        public User()
+        {
+            _id++;
+            Id = _id;
+        }
+    }
+}
---
+++ DelegatePracticePart2/GeneralPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using GeneralPractice.Models;
+using System;
+using Utils.Enums;
+
+namespace GeneralPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(typeof(User));
+            byte role;
+            bool isNum;
+            do
+            {
+                Console.WriteLine("Role (1. Admin, 2.Member):");
+                string roleStr = Console.ReadLine();
+                isNum = byte.TryParse(roleStr, out role);
+            } while (!isNum || !Enum.IsDefined(typeof(Role), role));
+        }
+    }
+}
